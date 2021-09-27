02a1e2b408be91fca2f84b54ffc8ce451833dada
Aram Garabedian <garabedian.aram@gmail.com>
2021-09-26 17:20:59 -0700

    Modified folder structure

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App/App';
 
 describe('Frontend test', () => {
     test('renders learn react link', () => {
