ad57c37c31394d2243414ffbab557ece171fcf32
Aram Garabedian <garabedian.aram@gmail.com>
2021-10-14 11:20:39 -0700

    Added tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { Button } from '@material-ui/core';
 import App from '../App/App';
 
 configure({ adapter: new Adapter() });
@@ -9,5 +10,52 @@ describe('Frontend test', () => {
     test('renders App', () => {
         const wrapper = mount(<App />);
         expect(wrapper).toMatchSnapshot();
+        wrapper.unmount();
+    });
+
+    test('Start clicked', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.find(App).state('clickedStart')).toEqual(false);
+
+        wrapper.find(Button).first().simulate('click');
+        wrapper.update();
+
+        expect(wrapper.find(App).state('clickedStart')).toEqual(true);
+        wrapper.unmount();
+    });
+
+    test('First Select change', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.find(App).state('firstCategoryValue')).toEqual('All');
+
+        wrapper.find(App).setState({ clickedStart: true });
+        const app = wrapper.find(App).instance() as App;
+        app.handleFirstCategoryChange('Pizza');
+        expect(wrapper.find(App).state('firstCategoryValue')).toEqual('Pizza');
+    });
+
+    test('Add category clicked', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.find(App).state('addCategory')).toEqual(false);
+
+        wrapper.find(App).setState({ clickedStart: true });
+        wrapper.find(App).setState({ firstCategoryValue: 'Burger' });
+        // Add category button is now the first node because Start was unrendered
+        wrapper.find(Button).first().simulate('click');
+        wrapper.update();
+
+        expect(wrapper.find(App).state('addCategory')).toEqual(true);
+        wrapper.unmount();
+    });
+
+    test('Second Select change', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.find(App).state('secondCategoryValue')).toEqual('');
+
+        wrapper.find(App).setState({ clickedStart: true });
+        wrapper.find(App).setState({ firstCategoryValue: 'Pizza' });
+        const app = wrapper.find(App).instance() as App;
+        app.handleSecondCategoryChange('Burgers');
+        expect(wrapper.find(App).state('secondCategoryValue')).toEqual('Burgers');
     });
 });
